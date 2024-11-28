85589dd630d1e07f67bb4c29ca2a1cb126c0c716
Michal <michalpelka@gmail.com>
2024-11-28 20:51:08 +0000

    Fixes to camera:
    - test photo to tmp
    - name with nanoseconds
    - setting low exposition
    
    Signed-off-by: Michal <michalpelka@gmail.com>

--- extras/picamera2/camera.py
+++ extras/picamera2/camera.py
@@ -15,11 +15,25 @@ picam2 = Picamera2()
 camera_config = picam2.create_still_configuration(main={"size": picam2.sensor_resolution})
 picam2.configure(camera_config)
 
+#Manual exposure time
+#picam2.set_controls({"AeEnable": 0, "ExposureTime": 25000})  # Example: 5 ms exposure
+
+# automated exposue, set to small value
+picam2.set_controls({"ExposureValue": -4})
+
+#
+#picam2.set_controls({"HdrMode":4});
 
 picam2.start()
+# List the controls and their properties
 
+controls = picam2.camera_controls
+print("Available controls:")
+for control, info in controls.items():
+    print(f"{control}: {info}")
+    
 print("Subscriber connected and listening for messages...")
-
+picam2.capture_file("/tmp/test.jpg")
 # Receive messages in a loop and capture an image for each
 while True:
     # Wait for a message
@@ -29,7 +43,7 @@ while True:
         data = json.loads(message)
 
         mode = data['mode']
-        ts = data['time']
+        ts = int(data['time'])
         data_continous = data['continousScanDirectory']
         
   
