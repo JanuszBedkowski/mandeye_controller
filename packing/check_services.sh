bbf7b6b71108efb909b775ba50ea130708deaf94
Michał Pełka <michalpelka@gmail.com>
2026-04-26 01:37:50 +0200

    Update check_services.sh

--- packing/check_services.sh
+++ packing/check_services.sh
@@ -8,7 +8,7 @@ services=(
   mandeye_ptp4l-gm-eth0.service
   mandeye_phc2sys-gm-eth0.service
   mandeye_extra_gnss.service
-  mandeye_extra_murata_uart_imu.service
+  mandeye_murata_uart_driver.service
 )
 
 echo "Checking Mandeye services..."
